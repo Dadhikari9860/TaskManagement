cb46d36f6ca0b452d767f2dca53331763f489f34
Dadhikari9860 <dineshadhikari660@gmail.com>
2026-01-02 14:55:13 +0545

    this is some small changes

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,15 +2,23 @@
 
 namespace App\Providers;
 
+use App\Models\Task;
+use App\Policies\TaskPolicy;
 use App\Models\User;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
+    protected $policies = [
+        Task::class => TaskPolicy::class,
+    ];
+
     public function boot(): void
     {
-        Gate::before(function (User $user, string $ability) {
+        $this->registerPolicies();
+
+        Gate::before(function (User $user) {
             return $user->isAdmin() ? true : null;
         });
     }
